9f9c2a8e771682ba6fd37ebdaf82a2f8ebb9f9e7
Kelly Dwan <kelly.dwan@gmail.com>
2016-07-09 22:32:16 -0400

    Intial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const lang = 'en';
+const width = 60;
+
+const Request = require( 'request-promise' );
+const find = require( 'lodash/find' );
+const wrap = require( 'wordwrap' )( width );
+
+const id = Math.floor( Math.random() * 722 );
+const options = {
+	uri: 'http://pokeapi.co/api/v2/pokemon-species/' + id + '/',
+	json: true // Automatically parses the JSON string in the response 
+};
+
+function getPokeSprite( sprites ) {
+	const frontPriority = [ 'front_shiny', 'front_default', 'front_shiny_female', 'front_female' ];
+
+	let currentSprite = frontPriority.pop();
+	while ( null === sprites[ currentSprite ] ) {
+		currentSprite = frontPriority.pop();
+	}
+	
+	return sprites[ currentSprite ];
+}
+
+function getLocalizedObject( names, language ) {
+	return find( names, ( name ) => {
+		return ( name.language.name === language );
+	} );
+}
+
+console.log( "Fetching a pokemon…" );
+
+Request( options ).then( ( response ) => {
+	let flavorText = getLocalizedObject( response.flavor_text_entries, lang ).flavor_text.replace( /\n/g, ' ' );
+	let title = getLocalizedObject( response.names, lang ).name;
+	let padding = new Array( Math.floor( ( width - title.length ) / 2 ) ).join( ' ' );
+
+	console.log( Array( width + 1 ).join( '=' ) );
+	console.log( padding, title );
+	console.log( Array( width + 1 ).join( '-' ) );
+	console.log( wrap( flavorText ) );
+	console.log( Array( width + 1 ).join( '=' ) );
+
+} ).catch( ( error ) => {
+	console.error( error );
+} );
